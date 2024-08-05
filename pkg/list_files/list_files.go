a07a3d40dc52a7bf409e2d4888fbbee6689f9fb7
Scott-Simmons <52365471+Scott-Simmons@users.noreply.github.com>
2024-08-05 16:59:47 +1000

    Create list google files logic and tests

---
+++ pkg/list_files/list_files.go
@@ -0,0 +1,70 @@
+package list_files
+
+import (
+  "encoding/json"
+  "fmt"
+  "log"
+	"SecureSyncDrive/pkg/rpc_client"
+  _ "github.com/rclone/rclone/fs/operations"
+)
+
+
+type listFilesRequest struct {
+  FSrc   string `json:"fs"`
+  Remote string `json:"remote"`
+}
+
+type GoogleDriveFile struct {
+  Path     string `json:"Path"`
+	Name     string `json:"Name"`
+	Size     int    `json:"Size"`
+	MimeType string `json:"MimeType"`
+	ModTime  string `json:"ModTime"`
+	IsDir    bool   `json:"IsDir"`
+	ID       string `json:"ID"`
+}
+
+type FileListOutput struct {
+	List []GoogleDriveFile `json:"list"`
+}
+
+// Generics don't work well in golang but would be lovely to have a generic map.
+func Map(files []GoogleDriveFile, f func(GoogleDriveFile) string) []string {
+	ids := make([]string, len(files))
+	for i, file := range files {
+		ids[i] = f(file)
+	}
+	return ids
+}
+
+func ListRemoteFiles(client rpc_client.RPCClient, googleDriveRemote string) ([]GoogleDriveFile, error) {
+  // Assume flat structure for now no recursive list. Will be flat encrypted anyway.
+  // Ref: https://github.com/rclone/rclone/blob/1901bae4ebcbc4cdd82f6bb1862d0479f3fa386e/fs/operations/rc_test.go
+	if err := client.Initialize(); err != nil {
+		return nil, err
+	}
+	const listJsonMethod = "operations/list"
+	listFilesRequest := listFilesRequest {
+		FSrc: googleDriveRemote,
+		Remote: "",
+	}
+	listFilesRequestJson, err := json.Marshal(listFilesRequest)
+	if err != nil {
+		return nil, err
+	}
+	out, status := client.RPC(listJsonMethod, string(listFilesRequestJson))
+	if status != 200 {
+		return nil, fmt.Errorf("Error status: %d and error output: %s", status, out)
+	}
+  var structuredOutput FileListOutput
+  err = json.Unmarshal([]byte(out), &structuredOutput)
+	if err != nil {
+		log.Fatalf("Error unmarshalling JSON: %v", err)
+	}
+
+	return structuredOutput.List, nil
+}
+
+func GetGoogleDriveFileIds(files []GoogleDriveFile) []string {
+  return Map(files, func(f GoogleDriveFile) string { return f.ID} )
+}
---
+++ pkg/list_files/list_files_test.go
@@ -0,0 +1,75 @@
+package list_files
+
+import (
+	"testing"
+  "reflect"
+	"github.com/stretchr/testify/mock"
+)
+
+// TODO: Get rid of mocking duplication later
+
+func GenGoogleDriveFilesTestData() []GoogleDriveFile {
+    return []GoogleDriveFile{
+        {
+            Path:     "foo/bar.txt",
+            Name:     "bar.txt",
+            Size:     5,
+            MimeType: "text/plain",
+            ModTime:  "2024-08-04T14:43:02.022Z",
+            IsDir:    false,
+            ID:       "1YImqqzHic8Acx0-4kyiENxki6i6Sgf5z",
+        },
+        {
+            Path:     "files/yoo.tar.gzip",
+            Name:     "yoo.tar.gzip",
+            Size:     160,
+            MimeType: "application/octet-stream",
+            ModTime:  "2024-08-04T14:49:55.776Z",
+            IsDir:    false,
+            ID:       "1RUD_wmTvjg9g-tFBjEbEFb2Sr2jXmgPi",
+        },
+    }
+}
+
+type MockLibrclone struct {
+	mock.Mock
+}
+
+type MockRPCClient struct {
+	mock.Mock
+}
+
+func (m *MockRPCClient) Initialize() error {
+	args := m.Called()
+	return args.Error(0)
+}
+
+func (m *MockRPCClient) RPC(method string, params string) (string, int) {
+	args := m.Called(method, params)
+	return args.String(0), args.Int(1)
+}
+
+// TODO: This is a weak test. Need to expand it... edge cases
+func TestListFiles(t *testing.T) {
+  // Needs a real integration test to test output
+	client := new(MockRPCClient)
+	client.On("Initialize", mock.Anything).Return(nil)
+	client.On("RPC", "operations/list", mock.Anything).Return(`{"success": {}}`, 200)
+  // TODO: Test output properly
+	_, err := ListRemoteFiles(client, "remote:")
+	if err != nil {
+		t.Errorf("List files failed: %v", err)
+	}
+	client.AssertExpectations(t)
+}
+
+func TestGetGoogleDriveFileIds(t *testing.T) {
+  expectedIds := []string {
+    "1YImqqzHic8Acx0-4kyiENxki6i6Sgf5z",
+    "1RUD_wmTvjg9g-tFBjEbEFb2Sr2jXmgPi",
+  }
+  actualIds := GetGoogleDriveFileIds(GenGoogleDriveFilesTestData())
+  if !reflect.DeepEqual(actualIds, expectedIds) {
+        t.Errorf("Expected IDs %v, but got %v", expectedIds, actualIds)
+  }
+}
