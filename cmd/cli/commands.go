e764876a8d1258a64bc369ce77ab567c60ff3799
Scott-Simmons <52365471+Scott-Simmons@users.noreply.github.com>
2024-08-17 23:02:27 +1000

    Renaming what is exposed to public via the CLI

--- cmd/cli/commands.go
+++ cmd/cli/commands.go
@@ -3,11 +3,12 @@ package main
 // Probably exposing too much to the user right now. But can reel things back later
 
 import (
-	"SecureSyncDrive/pkg/push"
 	"SecureSyncDrive/pkg/config_create"
 	"SecureSyncDrive/pkg/config_dump"
 	"SecureSyncDrive/pkg/decrypt"
+	"SecureSyncDrive/pkg/do_copy"
 	"SecureSyncDrive/pkg/encrypt"
+	"SecureSyncDrive/pkg/push"
 	"SecureSyncDrive/pkg/sync"
 	"fmt"
 	"log"
@@ -20,13 +21,14 @@ type Push struct {
 	BucketName string `help:"Name of the backblaze bucket to write directory to."`
 	RemoteName string `help:"Name of the backblaze remote."`
 }
+
 func (g *Push) Run() error {
 	fmt.Println("Starting archival, encryption, and syncing to backblaze.")
 	err := push.Push(
 		g.SrcDir,
-    g.BucketName,
+		g.BucketName,
 		g.PrivateKey,
-    g.RemoteName,
+		g.RemoteName,
 	)
 	if err != nil {
 		log.Fatalf("Error archiving and encrypting: %v", err)
@@ -38,6 +40,7 @@ func (g *Push) Run() error {
 type GenKeyCmd struct {
 	// Just to keep the pattern consistent.
 }
+
 func (g *GenKeyCmd) Run() error {
 	fmt.Println("Generating key...")
 	key, err := encrypt.GenerateKey()
@@ -49,12 +52,13 @@ func (g *GenKeyCmd) Run() error {
 }
 
 type SyncToBackblazeCmd struct {
-	FilePathToSync   string `help:"File to sync to remote."`
+	FilePathToSync      string `help:"File to sync to remote."`
 	BackblazeRemoteName string `help:"Name of the (backblaze) remote."`
 	BackblazeBucketName string `help:"Name of the (backblaze) bucket."`
 	// TODO: Maybe better to specify as a connection string for flexibilty
 	// See: https://rclone.org/docs/#connection-strings
 }
+
 func (s *SyncToBackblazeCmd) Run(globals *Globals) error {
 	fmt.Println("Syncing file to backblaze...")
 	fmt.Println("Don't forget to export RCLONE_CONFIG_PASS")
@@ -70,24 +74,27 @@ func (s *SyncToBackblazeCmd) Run(globals *Globals) error {
 	return nil
 }
 
-type SyncFromBackblazeCmd struct {
-	LocalSyncDir     string `help:"Directory to sync remote to"`
-	BackblazeRemoteName string `help:"Name of the (backblaze) remote"`
+type CopyFromBackblazeCmd struct {
+	LocalSyncDst            string `help:"Directory name to sync remote to"`
+	BackblazeRemoteFilePath string `help:"Name of the (backblaze) remote file"`
+	BackblazeRemoteName     string `help:"Name of the (backblaze) remote"`
+	BackblazeBucketName     string `help:"Name of the (backblaze) bucket"`
 	// TODO: Maybe better to specify as a connection string for flexibilty
 	// See: https://rclone.org/docs/#connection-strings
 }
-func (s *SyncFromBackblazeCmd) Run(globals *Globals) error {
+
+func (c *CopyFromBackblazeCmd) Run(globals *Globals) error {
 	fmt.Println("Syncing backblaze to local directory...")
 	fmt.Println("Don't forget to export RCLONE_CONFIG_PASS")
 	client, err := sync.NewClient()
 	if err != nil {
 		return err
 	}
-	err = sync.SyncFromBackblaze(client, s.BackblazeRemoteName, s.LocalSyncDir)
+	err = do_copy.CopyFromBackblaze(client, c.BackblazeRemoteFilePath, c.BackblazeRemoteName, c.BackblazeBucketName, c.LocalSyncDst)
 	if err != nil {
 		return err
 	}
-	fmt.Println("Syncing done. Check the local for changes.")
+	fmt.Println("Syncing done. Check local for changes.")
 	return nil
 }
 
@@ -95,6 +102,7 @@ type DecryptTarCmd struct {
 	SrcFile    string `help:"File to decrypt."`
 	PrivateKey string `help:"AES-256 private key file path."`
 }
+
 func (d *DecryptTarCmd) Run() error {
 	fmt.Println("Starting archival and encryption")
 	err := decrypt.Decrypt(
@@ -109,6 +117,7 @@ func (d *DecryptTarCmd) Run() error {
 }
 
 type ConfigDumpCmd struct{}
+
 func (c *ConfigDumpCmd) Run() error {
 	fmt.Println("Dumping rclone config...")
 	client, err := sync.NewClient()
@@ -121,6 +130,7 @@ func (c *ConfigDumpCmd) Run() error {
 }
 
 type ConfigCreateCmd struct{}
+
 func (c *ConfigCreateCmd) Run() error {
 	fmt.Println("creating rclone config...")
 	client, err := sync.NewClient()
